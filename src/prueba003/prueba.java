e961567abb0e25fdae7ff53ddbed01f4609c1fd4
emir2021 <39033629+emir2021@users.noreply.github.com>
2018-05-09 11:47:03 -0500

    peroo

---
+++ src/prueba003/prueba.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba003;
+
+/**
+ *
+ * @author Usuario
+ */
+public class prueba {
+    
+    
+    public void nada(){
+        System.out.println("Hola puto");
+        
+    }
+          
+    
+}
